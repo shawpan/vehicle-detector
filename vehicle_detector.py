d87a42e53cd7788a92e4a68aeb038dafd3ebe325
Akiz <shawpan.du@gmail.com>
2017-02-19 02:11:30 +0600

    feat: Add draw boxes

--- vehicle_detector.py
+++ vehicle_detector.py
@@ -51,6 +51,23 @@ class VehicleDetector:
         # Return the list of windows
         return window_list
 
+    def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+        """ Draw boxes
+        Attr:
+            img: image to draw boxes on
+            bboxes: bounding boxes of given windows
+            color: color object
+            thick: thickness of box line
+        """
+        # Make a copy of the image
+        imcopy = np.copy(img)
+        # Iterate through the bounding boxes
+        for bbox in bboxes:
+            # Draw a rectangle given bbox coordinates
+            cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+        # Return the image copy with boxes drawn
+        return imcopy
+
 
     def process_image(self):
         pass
