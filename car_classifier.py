bfa67217564376ce1dc38ee5f8b2331012a7fe3d
Akiz <shawpan.du@gmail.com>
2017-02-18 23:46:51 +0600

    feat: Add get_data method
    Added method for preparing train and test data in car classifier

--- car_classifier.py
+++ car_classifier.py
@@ -3,6 +3,10 @@ import cv2
 import glob
 import pickle
 import os
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
 
 class CarClassifier:
     """ Classifier for car object
@@ -48,7 +52,7 @@ class CarClassifier:
         for img_path in img_paths:
             feature = []
             img = cv2.imread(img_path)
-            color_trans_img = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+            color_trans_img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
             img_shape = color_trans_img.shape
 
             # Spatial feature
@@ -60,7 +64,7 @@ class CarClassifier:
             # Hog feature
             hog_features = []
             for channel in range(img_shape[2]):
-                hog_feature = hog(color_trans_img[:,:,channel, orientations=8,
+                hog_feature = hog(color_trans_img[:,:,channel], orientations=8,
                        pixels_per_cell=(8, 8),
                        cells_per_block=(2, 2),
                        transform_sqrt=True,
@@ -72,3 +76,52 @@ class CarClassifier:
             features.append(np.concatenate(feature))
 
         return features
+
+    def get_data(self):
+        """ Prepare train and test data set from car and not car images
+        Returns:
+            (x_train, y_train, x_test, y_test)
+        """
+        print("Preparing train and test data...")
+        # If data exists then return
+        if os.path.isfile('data.p'):
+            with open('data.p', 'rb') as data_file:
+                data = pickle.load(data_file)
+            print("Done preparing train and test data")
+            return (data['x_train'], data['y_train'], data['x_test'], data['y_test'])
+
+        car_images = glob.glob(self.car_img_dir + '/**/*.png', recursive=True)
+        car_images = car_images[:self.sample_size]
+
+        not_car_images = glob.glob(self.not_car_img_dir + '/**/*.png', recursive=True)
+        not_car_images = not_car_images[:self.sample_size]
+
+        car_features = self.get_features(car_images)
+        not_car_features = self.get_features(not_car_images)
+
+        features = np.vstack((car_features, not_car_features)).astype(np.float64)
+        scaler = StandardScaler().fit(features)
+        scaled_features = scaler.transform(features)
+        labels = np.hstack((np.ones(len(car_features)), np.zeros(len(not_car_features))))
+        x_train, x_test, y_train, y_test = train_test_split(
+            scaled_features, labels, test_size=0.2, random_state=np.random.randint(0, 100))
+
+        data = {
+            'x_train' : x_train,
+            'y_train' : y_train,
+            'x_test'  : x_test,
+            'y_test'  : y_test
+        }
+        # Save as a file
+        with open('data.p', "wb") as data_file:
+            pickle.dump(data, data_file)
+        print("Done preparing train and test data")
+
+        return (x_train, y_train, x_test, y_test)
+
+    def fit(self):
+        """ Fit classifier to car and not car data
+        """
+        x_train, y_train, x_test, y_test = self.get_data()
+        print('Training set : ', x_train.shape)
+        print('Test set : ', x_test.shape)
